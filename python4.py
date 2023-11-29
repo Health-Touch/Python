fec1f193938dd6ede1fa9a9bfa1f0816652634b3
iHx7z <anthony.ferreira@sptech.school>
2023-11-29 04:36:31 -0300

    python4

--- python4.py
+++ python4.py
@@ -6,7 +6,6 @@ from mysql.connector import connect
 import requests
 import json
 import pymssql
-import pyodbc
 
 # Função para obter a conexão com o banco de dados
 def mysql_connection(host, user, passwd, database=None):
@@ -17,16 +16,10 @@ def mysql_connection(host, user, passwd, database=None):
         database=database
     )
     return connection
-
-def sql_server_connection(server, database, username, password):
-    conn_str = f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
-    connection = pyodbc.connect(conn_str)
-    return connection
-
 # Conectar ao banco de dados
-connection = mysql_connection('localhost', 'root', 'Biel0501', 'HealthTouch')
+connection = mysql_connection('localhost', 'root', 'urubu100', 'HealthTouch')
 
-sql_server_connection = sql_server_connection('54.145.218.19', 'HealthTouch', 'sa', 'urubu100')
+sql_server_connection = pymssql.connect(server='54.145.218.19', database='HealthTouch', user='sa', password='urubu100')
 
 def insert_data(connection, query, values):
     cursor = sql_server_connection.cursor()
@@ -51,7 +44,6 @@ query = "SELECT nome FROM Colaborador WHERE email = %s AND senha = %s"
 #cursor.execute(query, (email, senha))
 cursor.execute(query, (email, senha))
 nome = cursor.fetchone()
-
 # puxando a fkEmpresa
 query = "SELECT fkEmpresa FROM Colaborador WHERE email = %s AND senha = %s"
 #cursor.execute(query, (email, senha))
@@ -89,7 +81,7 @@ if resultado:
             passwd=passwd,
             database=database
         )
-        return connection
+  return connection
 
     # aqui colocar suas credencias do banco
     #connection = mysql_connection('localhost', 'root', 'Biel0501', 'HealthTouch')
@@ -131,17 +123,17 @@ if resultado:
         query = "select idMaquina, SO, IP, sala, andar, nome from Maquina JOIN LocalSala on fkLocal = idLocalSala join setor on fkSetor = idSetor WHERE idMaquina = %s;"
         cursor.execute(query, (idMaquinaSelect,))
         DadosMaquinas = cursor.fetchone()
-        
+
         # rodando o monitoramento
         while True:
-            uso_cpu = round(psutil.cpu_percent(interval=1), 2)
+ uso_cpu = round(psutil.cpu_percent(interval=1), 2)
             uso_disco = round(psutil.disk_usage('/').percent, 2)
             uso_memoria = round(psutil.virtual_memory().percent, 2)
             data = datetime.datetime.now()
 
 
             webhook = "https://hooks.slack.com/services/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"
-           
+
             if 15.0 <= uso_cpu < 30.0:
                 alerta = {"text": f"Alerta! CPU com {uso_cpu}% de uso, Maquina com o IP: {DadosMaquinas[2]}, andar: {DadosMaquinas[4]}, sala: {DadosMaquinas[3]}" }
                 requests.post(webhook, data=json.dumps(alerta))
@@ -155,16 +147,16 @@ if resultado:
             elif uso_memoria >= 50.0:
                 alerta = {"text": f"Crítico! RAM com {uso_memoria}% de uso, Maquina com o IP: {DadosMaquinas[2]}, andar: {DadosMaquinas[4]}, sala: {DadosMaquinas[3]}"}
                 requests.post(webhook, data=json.dumps(alerta))
-                
+
             if 65.0 <= uso_disco < 70.0:
                 alerta = {"text": f"Alerta! Disco com {uso_disco}% de uso, Maquina com o IP: {DadosMaquinas[2]}, andar: {DadosMaquinas[4]}, sala: {DadosMaquinas[3]}"}
                 requests.post(webhook, data=json.dumps(alerta))
             elif uso_disco >= 70.0:
                 alerta = {"text": f"Crítico! RAM com {uso_disco}% de uso, Maquina com o IP: {DadosMaquinas[2]}, andar: {DadosMaquinas[4]}, sala: {DadosMaquinas[3]}"}
                 requests.post(webhook, data=json.dumps(alerta))
-                
-                
-            
+
+
+
             #query = '''
             #insert into Monitoramento(porcentagem, dataHora, fkComponente, fkMaquina, fkPlanoEmpresa, fkTipoMaquina, fkEmpresaMaquina)
             #VALUES (%s, %s, %s, %s, %s, %s, %s), (%s, %s, %s, %s, %s, %s, %s), (%s, %s, %s, %s, %s, %s, %s);
@@ -176,10 +168,10 @@ if resultado:
             #]
 
             #cursorSql = connectionSql.cursor()
-            #cursorSql.execute(query, insert)
+  #cursorSql.execute(query, insert)
             #connectionSql.commit()
 
-            
+
 
 
             query = '''
@@ -189,10 +181,10 @@ if resultado:
 
             queryServer = '''
             insert into Monitoramento(porcentagem, dataHora, fkComponente, fkMaquina, fkPlanoEmpresa, fkTipoMaquina, fkEmpresaMaquina)
-            VALUES (?, ?, ?, ?, ?, ?, ?);
-    
+            VALUES (%s, %s, %s, %s, %s, %s, %s);
+
             '''
-            
+
             insert_values_cpu = (
                 uso_cpu, data, 1, idMaquinaInsert[0], fkPlanoEmpresa[0], fkTipoMaquina[0], fkEmpresa[0],
                 #uso_disco, data, 2, idMaquinaInsert[0], fkPlanoEmpresa[0], fkTipoMaquina[0], fkEmpresa[0],
@@ -212,13 +204,13 @@ if resultado:
             )
 
 
-           
+
             insert_values_sql_server_cpu = (
                 uso_cpu, data, 1, idMaquinaInsert[0], fkPlanoEmpresa[0], fkTipoMaquina[0], fkEmpresa[0],
                 #uso_disco, data, 2, idMaquinaInsert[0], fkPlanoEmpresa[0], fkTipoMaquina[0], fkEmpresa[0],
                 #uso_memoria, data, 3, idMaquinaInsert[0], fkPlanoEmpresa[0], fkTipoMaquina[0], fkEmpresa[0],
                 # Adicione mais conjuntos de valores conforme necessário
-            )
+   )
 
             insert_values_sql_server_ram = (
                 #uso_cpu, data, 1, idMaquinaInsert[0], fkPlanoEmpresa[0], fkTipoMaquina[0], fkEmpresa[0],
@@ -226,7 +218,7 @@ if resultado:
                 uso_memoria, data, 3, idMaquinaInsert[0], fkPlanoEmpresa[0], fkTipoMaquina[0], fkEmpresa[0],
                 # Adicione mais conjuntos de valores conforme necessário
             )
-                
+
             insert_values_sql_server_disco = (
                 #uso_cpu, data, 1, idMaquinaInsert[0], fkPlanoEmpresa[0], fkTipoMaquina[0], fkEmpresa[0],
                 uso_disco, data, 2, idMaquinaInsert[0], fkPlanoEmpresa[0], fkTipoMaquina[0], fkEmpresa[0],
@@ -237,11 +229,11 @@ if resultado:
 
 
             #print(insert_values, insert_values_sql_server)
-        
+
             insert_data(sql_server_connection, queryServer, insert_values_sql_server_cpu)
             insert_data(sql_server_connection, queryServer, insert_values_sql_server_ram)
             insert_data(sql_server_connection, queryServer, insert_values_sql_server_disco)
-    
+
             # Criar cursor
             cursor = connection.cursor()
 
@@ -250,16 +242,14 @@ if resultado:
             cursor.execute(query, insert_values_ram)
             cursor.execute(query, insert_values_disco)
             connection.commit()
-            
+
             print(f"Uso da CPU: {uso_cpu}%")
             print(f"Uso do Disco: {uso_disco}%")
             print(f"Uso da Memória: {uso_memoria}%\r\n")
 
             time.sleep(5)
-
-    else:
+ else:
         print("Máquina não está cadastrada")
 
 else:
     print("Login Inválido")
-  
